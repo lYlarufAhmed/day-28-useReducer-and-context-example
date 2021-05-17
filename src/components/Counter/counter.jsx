8bfde3175e2e1d447f4cb7b451611d3ed3713c17
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-05-17 17:00:38 +0600

    fixed

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,4 +1,4 @@
-import {useContext, useState} from "react";
+import {useContext} from "react";
 import styled from "styled-components";
 import CounterContext from "../../CounterStore";
 const CounterWrapper = styled.div`
